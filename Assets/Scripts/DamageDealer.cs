236fd5a33d4d156a01343d4e7995194cc34cb72b
Teodora Vela <teodora11vela@gmail.com>
2025-05-27 16:26:17 +0300

    added health and damage

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 10;
+    public int GetDamage()
+    {
+        return damage;
+    }
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+        if (damageDealer != null)
+        {
+            TakeDamage(damageDealer.GetDamage());
+            damageDealer.Hit();
+        }
+
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
